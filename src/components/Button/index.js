71b0d41e8d46b450bd1c972a6f1d6e312048fc86
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2021-12-21 11:20:17 -0300

    feat: componente button

---
+++ src/components/Button/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button`
+  background-color: ${(props) => props.backgroundColor};
+  width: ${(props) => props.width};
+  height: 45px;
+  color: var(--white);
+  border-radius: ${(props) => props.borderRadius};
+  font-size: 24px;
+  padding: 5px;
+  margin: ${(props) => props.margin};
+  :hover {
+    background-color: ${(props) => props.colorHover};
+  }
+`;
